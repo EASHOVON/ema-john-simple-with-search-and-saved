9e7e43defe289b2cf9b6ca1dbe960e34df01833b
EASHOVON <ashrafuzzaman.shovon.786@gmail.com>
2021-10-01 14:17:46 +0600

    React Router Added

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NotFound = () =>
+{
+    return (
+        <div>
+            <h1>404</h1>
+            <h4>This page is not found</h4>
+        </div>
+    );
+};
+
+export default NotFound;
