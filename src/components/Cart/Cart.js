6e35844a3f16ded3f7cddfc9e0d7b9543df1a3b2
EASHOVON <ashrafuzzaman.shovon.786@gmail.com>
2021-10-02 00:21:02 +0600

    Something Error

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import './Cart.css';
 
-const Cart = (props) => {
+const Cart = (props) =>
+{
     const { cart } = props;
 
     // const totalReducer = (previous, product) => previous + product.price;
     // const total = cart.reduce(totalReducer, 0);
     let totalQuantity = 0;
     let total = 0;
-    for (const product of cart) {
-        if (!product.quantity) {
+    for (const product of cart)
+    {
+        if (!product.quantity)
+        {
             product.quantity = 1;
         }
         total = total + product.price * product.quantity;
--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import useProduct from '../../hooks/useProduct';
+import useCart from '../../hooks/useCart';
+import Cart from '../Cart/Cart';
 
-const OrderReview = () => {
+const OrderReview = () =>
+{
+    const [products, setProducts] = useProduct();
+    const [cart] = useCart(products);
     return (
         <div>
+            <h1>{products?.length}</h1>
+            <h3>{cart?.length}</h3>
             <h2>This is Order Review</h2>
+            <Cart cart={cart}></Cart>
         </div>
     );
 };
